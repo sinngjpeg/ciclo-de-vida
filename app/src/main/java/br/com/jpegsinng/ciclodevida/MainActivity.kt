1c99c283cb91780420946ac8b46ec194d5cb3efd
Ingrid Silva <ingridsilvatec@outlook.com>
2025-01-29 07:44:17 -0300

    add project

---
+++ app/src/main/java/br/com/jpegsinng/ciclodevida/MainActivity.kt
@@ -0,0 +1,32 @@
+package br.com.jpegsinng.ciclodevida
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import br.com.jpegsinng.ciclodevida.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val editAnimal = binding.editAnimal
+        val editBreed = binding.editBreed
+        val buttonSend = binding.buttonSend
+
+        buttonSend.setOnClickListener {
+            val animal = editAnimal.text.toString()
+            val breed = editBreed.text.toString()
+            val intent = Intent(this, ResultActivity::class.java)
+            if (editAnimal.text.isEmpty() || editBreed.text.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show()
+            } else {
+                intent.putExtra("animal", animal)
+                intent.putExtra("breed", breed)
+                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/br/com/jpegsinng/ciclodevida/ResultActivity.kt
@@ -0,0 +1,19 @@
+package br.com.jpegsinng.ciclodevida
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.jpegsinng.ciclodevida.databinding.ActivityResultBinding
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animal = intent.getStringExtra("animal") ?: "desconhecida"
+        val breed = intent.getStringExtra("breed") ?: "desconhecida"
+        binding.textMessage.text = {
+            getString(R.string.result_screen, animal, breed)
+        }.toString()
+    }
+}
